e2091dad683f5e364202861479247f12b92e26fd
hoang loc <hoangconglock15@gmail.com>
2025-05-05 16:56:20 +0700

    them types tra ve

--- packages/db/src/types/types.ts
+++ packages/db/src/types/types.ts
@@ -36,9 +36,10 @@ export interface IUser {
 // User With Role
 export interface AuthUser {
   id: string;
-  email: string;
   firstName: string;
   lastName: string;
+  email: string;
+  role_id: string;
   role: {
     id: string;
     name: string;
@@ -47,6 +48,31 @@ export interface AuthUser {
       action: string;
     }[];
   };
+}
+
+export interface SupabaseUserRow {
+  id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  status: string;
+  role_id: string;
+  role:
+    | {
+        id: string;
+        name: string;
+        permissions: { id: string; action: string }[];
+      }
+    | {
+        id: string;
+        name: string;
+        permissions: { id: string; action: string }[];
+      }[];
+}
+
+export interface ResponeAuthUser {
+  status: boolean;
+  message: string;
 }
 
 // Request/Response Types
