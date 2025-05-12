70828cb95fb6d7aed9721e6942235bcc40d53a0c
hoang loc <hoangconglock15@gmail.com>
2025-05-12 10:16:49 +0700

    update

--- apps/nextjs/src/app/login/page.tsx
+++ apps/nextjs/src/app/login/page.tsx
@@ -1,15 +1,5 @@
-import { Suspense } from "react";
-
 import Page from "./_components/page-client";
 
 export default function LoginPage() {
-  return (
-    <Suspense
-      fallback={
-        <div className="p-6 text-center">Đang tải trang đăng nhập...</div>
-      }
-    >
-      <Page />
-    </Suspense>
-  );
+  return <Page />;
 }
